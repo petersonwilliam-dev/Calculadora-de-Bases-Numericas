3a302efd24aa4ac879ecaa7ec819838d90e373cf
Peterson William <petersondev.profissional@gmail.com>
2023-12-12 08:24:33 -0300

    Adicionando função para converter de binário para decimal

---
+++ conversores/conversor_decimal/converterDecimal.py
@@ -0,0 +1,13 @@
+def converterBinarioDecimal(numero):
+
+    numeroConvertido = 0
+    numeroPotencia = 0
+
+    numero = numero[::-1]
+
+    for algarismo in numero:
+        numeroConvertido += int(algarismo) * (2**numeroPotencia)
+
+        numeroPotencia += 1
+
+    return numeroConvertido
